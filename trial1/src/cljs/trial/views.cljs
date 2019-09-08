97688676b740ee55d2cbd04684f2f2e49fdf7b88
swati <swatichauhanhjs17@gmail.com>
2019-09-08 10:18:14 +0530

    8-9-19

--- trial1/src/cljs/trial/views.cljs
+++ trial1/src/cljs/trial/views.cljs
@@ -1,38 +1,38 @@
 (ns trial.views
   (:require
-   [re-frame.core :as re-frame]
-   [trial.subs :as subs]
-   [reagent.core :as r]))
-
-
-
-
-
-
+    [re-frame.core :as re-frame]
+    [trial.subs :as subs]
+    [reagent.core :as r]))
 
 (defn my-country
-     [final] [:input
-               { :type "text"
-                 :value (get @final :country)
-                 :on-change #(swap! final assoc :country(-> % .-target .-value))}])
+  [final] [:input
+           {:type      "text"
+            :value     (get @final :country)
+            :on-change #(swap! final assoc :country (-> % .-target .-value))}])
 
 (defn my-identity
-    [final] [:input
-             {:type      "text"
-              :value     (get @final :identity)
-              :on-change #(swap! final assoc :identity(-> % .-target .-value))}])
+  [final] [:input
+           {:type      "text"
+            :value     (get @final :identity)
+            :on-change #(swap! final assoc :identity (-> % .-target .-value))}])
 
 (defn my-form
   []
   (let [final (r/atom {:identity "  NAME"
-                        :country " country"})]
-      (fn []
-          [:div
-            [:h3 [:span {:style {:color "red"}} "Enter your name :- " [my-identity final]]]
-            [:h2 [:span {:style {:color "blue"}} "Enter your country :- " [my-country final]]]
-            [:input {:type "button"
-                     :value "submit"
-                     :on-click #(re-frame/dispatch [:submit @final])}]])))
+                       :country  " country"})]
+    (fn []
+      [:div
+       [:h3 [:span {:style {:color "red"}} "Enter your name :- " [my-identity final]]]
+       [:h2 [:span {:style {:color "blue"}} "Enter your country :- " [my-country final]]]
+       [:input {:type     "button"
+                :value    "submit"
+                :on-click #(re-frame/dispatch [:submit @final])}]])))
+
+
+
+
+
+
 
 
 
@@ -41,21 +41,22 @@
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])
-        all-values(re-frame/subscribe [::subs/all-values])
-        last-submitted(re-frame/subscribe [::subs/last-submitted])
+        all-values (re-frame/subscribe [::subs/all-values])
+        last-submitted (re-frame/subscribe [::subs/last-submitted])
         ]
 
-    [:div  [:h1 "Hello from " @name]
+
+    [:div [:h1 "Hello from " @name]
 
 
      [my-form]
-     [:p "THIS IS THE RECENT DATA \n  :-" @all-values]
 
-     ;;
-     (for [item @all-values]
-       ^{:key item} [:li "Item " item])
+     [:p "Recent Data :- " (last @all-values)]
+     
+     [:ol (for [item @all-values]
+            ^{:key (str item)} [:li "DATA OF THE FORM :- " (str item)])
+      ]
+
      ]
-
-
 
     ))
